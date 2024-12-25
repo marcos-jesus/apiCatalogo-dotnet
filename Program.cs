d28bc9d634ba48009c4d748126796189136331d5
Marcos Jesus <marcosjesus.dev@gmail.com>
2024-12-25 05:10:13 -0300

    Adicionado relacionamento entre tabelas

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
+using api_dotnet.src.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- src/Model/Categoria.cs
+++ src/Model/Categoria.cs
@@ -1,8 +1,16 @@
+using System.Collections.ObjectModel;
+
 namespace api_dotnet.src.Model;
 
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
     public int Id { get; set; }
     public string? Nome { get; set; }
     public string? ImagemUrl { get; set; }
+
+    public ICollection<Produto>? Produtos { get; set; }
 }
